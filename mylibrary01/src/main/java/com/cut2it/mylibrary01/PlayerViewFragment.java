9e104bc99edcd4d2733feb4ca85953bb479553ad
Unknown <naffu.aziz@gmail.com>
2019-05-25 17:27:20 +0530

    lib pushed.

---
+++ mylibrary01/src/main/java/com/cut2it/mylibrary01/PlayerViewFragment.java
@@ -0,0 +1,28 @@
+package com.cut2it.mylibrary01;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlayerViewFragment extends Fragment {
+
+    public PlayerViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_player_view, container, false);
+
+        return view;
+    }
+
+
+}
